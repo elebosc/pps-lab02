916faa0e240fbbbab9d8f15ec6b83767358201d0
Elena Boschetti <elena.boschetti@studio.unibo.it>
2026-03-11 15:02:07 +0100

    [t2, 4] simplify first predicate, add tests and implementations of other predicates

--- src/main/scala/it/unibo/pps/task2/Currying.scala
+++ src/main/scala/it/unibo/pps/task2/Currying.scala
@@ -2,6 +2,12 @@ package it.unibo.pps.task2
 
 object Currying:
 
-  def p1(x: Int)(y: Int)(z: Int): Boolean = (x, y, z) match
-    case (a, b, c) if (a <= b && b == c) => true
-    case _ => false
+  val p1: Int => Int => Int => Boolean =
+    x => y => z => (x <= y && y == z)
+
+  val p2: (Int, Int, Int) => Boolean =
+    (x, y, z) => (x <= y && y == z)
+
+  def p3(x: Int)(y: Int)(z: Int): Boolean = (x <= y && y == z)
+
+  def p4(x: Int, y: Int, z: Int): Boolean = (x <= y && y == z)
--- src/test/scala/task2/CurryingTest.scala
+++ src/test/scala/task2/CurryingTest.scala
@@ -1,19 +1,79 @@
 package task2
 
-import it.unibo.pps.task2.Currying.p1
+import it.unibo.pps.task2.Currying.{p1, p2, p3, p4}
 import org.junit.Assert.{assertFalse, assertTrue}
 import org.junit.Test
 
 class CurryingTest:
 
-  @Test def testP1DetectsWhenRelationIsRespected(): Unit =
+  @Test def testP1DetectsWhenWholeRelationIsRespected(): Unit =
     val x = 1
     val y = 2
     val z = 2
     assertTrue(p1(x)(y)(z))
 
-  @Test def testP1DetectsWhenRelationIsNotRespected(): Unit =
+  @Test def testP1DetectsWhenFirstRelationIsNotRespected(): Unit =
+    val x = 3
+    val y = 2
+    val z = 2
+    assertFalse(p1(x)(y)(z))
+
+  @Test def testP1DetectsWhenSecondRelationIsNotRespected(): Unit =
     val x = 1
     val y = 2
     val z = 3
     assertFalse(p1(x)(y)(z))
+
+  @Test def testP2DetectsWhenWholeRelationIsRespected(): Unit =
+    val x = 1
+    val y = 2
+    val z = 2
+    assertTrue(p2(x, y, z))
+
+  @Test def testP2DetectsWhenFirstRelationIsNotRespected(): Unit =
+    val x = 3
+    val y = 2
+    val z = 2
+    assertFalse(p2(x, y, z))
+
+  @Test def testP2DetectsWhenSecondRelationIsNotRespected(): Unit =
+    val x = 1
+    val y = 2
+    val z = 3
+    assertFalse(p2(x, y, z))
+
+  @Test def testP3DetectsWhenWholeRelationIsRespected(): Unit =
+    val x = 1
+    val y = 2
+    val z = 2
+    assertTrue(p3(x)(y)(z))
+
+  @Test def testP3DetectsWhenFirstRelationIsNotRespected(): Unit =
+    val x = 3
+    val y = 2
+    val z = 2
+    assertFalse(p3(x)(y)(z))
+
+  @Test def testP3DetectsWhenSecondRelationIsNotRespected(): Unit =
+    val x = 1
+    val y = 2
+    val z = 3
+    assertFalse(p3(x)(y)(z))
+
+  @Test def testP4DetectsWhenWholeRelationIsRespected(): Unit =
+    val x = 1
+    val y = 2
+    val z = 2
+    assertTrue(p4(x, y, z))
+
+  @Test def testP4DetectsWhenFirstRelationIsNotRespected(): Unit =
+    val x = 3
+    val y = 2
+    val z = 2
+    assertFalse(p4(x, y, z))
+
+  @Test def testP4DetectsWhenSecondRelationIsNotRespected(): Unit =
+    val x = 1
+    val y = 2
+    val z = 3
+    assertFalse(p4(x, y, z))
