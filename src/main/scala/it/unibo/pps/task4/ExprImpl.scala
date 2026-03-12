95e15fde5248299e50da5708f73dfe3997849044
Elena Boschetti <elena.boschetti@studio.unibo.it>
2026-03-12 20:07:44 +0100

    [t4] add test cases and handle wrapping into parenthesis when necessary

--- src/main/scala/it/unibo/pps/task4/ExprImpl.scala
+++ src/main/scala/it/unibo/pps/task4/ExprImpl.scala
@@ -14,8 +14,12 @@ object ExprImpl:
       case Expr.Multiply(e1, e2) => evaluate(e1) * evaluate(e2)
 
   def show(expr: Expr): String =
+    def _wrapIntoParensIfNecessary(expr: Expr): String =
+      expr match
+        case Expr.Add(_, _) => s"(${show(expr)})"
+        case _ => s"${show(expr)}"
     expr match
       case Expr.Literal(n) => s"$n"
       case Expr.Add(e1, e2) => s"${show(e1)} + ${show(e2)}"
-      case Expr.Multiply(e1, e2) => s"${show(e1)} * ${show(e2)}"
-      
+      case Expr.Multiply(e1, e2) =>
+        s"${_wrapIntoParensIfNecessary(e1)} * ${_wrapIntoParensIfNecessary(e2)}"
--- src/test/scala/task4/ExprTest.scala
+++ src/test/scala/task4/ExprTest.scala
@@ -18,7 +18,7 @@ class ExprTest:
     val r = s"$n"
     assertEquals(r, show(e))
 
-  @Test def testAddIsEvaluatedCorrectly(): Unit = {
+  @Test def testAddIsEvaluatedCorrectly(): Unit =
     val n1 = 2
     val n2 = 3
     val e1 = Literal(n1)
@@ -26,7 +26,6 @@ class ExprTest:
     val e = Add(e1, e2)
     val r = n1 + n2
     assertEquals(r, evaluate(e))
-  }
 
   @Test def testAddIsShownCorrectly(): Unit =
     val n1 = 2
@@ -67,7 +66,7 @@ class ExprTest:
     val r = s"$n1 + $n2 + $n3 + $n4"
     assertEquals(r, show(e))
 
-  @Test def testMultiplyIsEvaluatedCorrectly(): Unit = {
+  @Test def testMultiplyIsEvaluatedCorrectly(): Unit =
     val n1 = 2
     val n2 = 3
     val e1 = Literal(n1)
@@ -75,7 +74,6 @@ class ExprTest:
     val e = Multiply(e1, e2)
     val r = n1 * n2
     assertEquals(r, evaluate(e))
-  }
 
   @Test def testMultiplyIsShownCorrectly(): Unit =
     val n1 = 2
@@ -114,4 +112,64 @@ class ExprTest:
     val e6 = Multiply(e3, e4)
     val e = Multiply(e5, e6)
     val r = s"$n1 * $n2 * $n3 * $n4"
+    assertEquals(r, show(e))
+
+  @Test def testAddIsRecursivelyEvaluatedCorrectlyWhenSubexpressionsAreMultiplications(): Unit =
+    val n1 = 2
+    val n2 = 3
+    val n3 = 4
+    val n4 = 5
+    val e1 = Literal(n1)
+    val e2 = Literal(n2)
+    val e3 = Literal(n3)
+    val e4 = Literal(n4)
+    val e5 = Multiply(e1, e2)
+    val e6 = Multiply(e3, e4)
+    val e = Add(e5, e6)
+    val r = n1 * n2 + n3 * n4
+    assertEquals(r, evaluate(e))
+
+  @Test def testAddIsRecursivelyShownCorrectlyWhenSubexpressionsAreMultiplications(): Unit =
+    val n1 = 2
+    val n2 = 3
+    val n3 = 4
+    val n4 = 5
+    val e1 = Literal(n1)
+    val e2 = Literal(n2)
+    val e3 = Literal(n3)
+    val e4 = Literal(n4)
+    val e5 = Multiply(e1, e2)
+    val e6 = Multiply(e3, e4)
+    val e = Add(e5, e6)
+    val r = s"$n1 * $n2 + $n3 * $n4"
+    assertEquals(r, show(e))
+
+  @Test def testMultiplyIsRecursivelyEvaluatedCorrectlyWhenSubexpressionsAreAdditions(): Unit =
+    val n1 = 2
+    val n2 = 3
+    val n3 = 4
+    val n4 = 5
+    val e1 = Literal(n1)
+    val e2 = Literal(n2)
+    val e3 = Literal(n3)
+    val e4 = Literal(n4)
+    val e5 = Add(e1, e2)
+    val e6 = Add(e3, e4)
+    val e = Multiply(e5, e6)
+    val r = (n1 + n2) * (n3 + n4)
+    assertEquals(r, evaluate(e))
+
+  @Test def testMultiplyIsRecursivelyShownCorrectlyWhenSubexpressionsAreAdditions(): Unit =
+    val n1 = 2
+    val n2 = 3
+    val n3 = 4
+    val n4 = 5
+    val e1 = Literal(n1)
+    val e2 = Literal(n2)
+    val e3 = Literal(n3)
+    val e4 = Literal(n4)
+    val e5 = Add(e1, e2)
+    val e6 = Add(e3, e4)
+    val e = Multiply(e5, e6)
+    val r = s"($n1 + $n2) * ($n3 + $n4)"
     assertEquals(r, show(e))
